ef88ff57a7e2c077a328e9a6430895358dab563f
Puria Kordrostami <kordeviant@gmail.com>
2022-11-29 14:05:14 +0330

    fixes

--- packages/my-lib/src/App.tsx
+++ packages/my-lib/src/App.tsx
@@ -1,18 +1,57 @@
-import styled from 'styled-components';
-import MyButton from './lib/MyButton';
+import { useEffect, useState } from "react";
+import { Atom, useAtomSetState, useAtomValue } from "./lib";
 
-function App() {
+const stateAtom = new Atom({ a: 1, b: 1 });
+
+const Menu = () => {
+    const setState = useAtomSetState(stateAtom);
+    useEffect(() => {
+      setInterval(() => {
+        setState((x) => ({ a: x.a + 1, b: x.b + 1 }));
+      }, 1000);
+    }, []);
     return (
-        <Wrapper>
-            <MyButton>Click me</MyButton>
-        </Wrapper>
+      <button
+        onClick={() => {
+          setState((x) => ({ a: x.a + 1, b: x.b + 1 }));
+        }}
+      >
+        test
+      </button>
     );
-}
-
-const Wrapper = styled.div`
-    margin: 2rem;
-    display: flex;
-    justify-content: center;
-`;
-
-export default App;
+  };
+  
+  const ShowState = ({ t }) => {
+    const state = useAtomValue(stateAtom);
+    return <div>{state[t]}</div>;
+  };
+  
+  function App() {
+    const [show, setShow] = useState(true);
+    useEffect(() => {
+      setTimeout(() => {
+        setShow(false);
+      }, 10000);
+    });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Menu />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {show && <ShowState t={"a"} />}
+          <ShowState t={"b"} />
+        </header>
+      </div>
+    );
+  }
+  export default App;
--- packages/my-lib/src/lib/index.ts
+++ packages/my-lib/src/lib/index.ts
@@ -1,1 +1,75 @@
-export { default as MyButton } from './MyButton';
+import { useCallback, useEffect, useRef, useState } from 'react'
+type Updater<T> = (arg1: T) => void;
+type SetAtomState<T> = (arg1: T) => T;
+
+export class Atom<T> {
+  subscribers: {
+    [key: number]: Updater<T>;
+  } = {}
+
+  value: T
+  constructor (value: T) {
+    this.value = value
+  }
+
+  subscribeAnyFunction (fn: Updater<T>) {
+    const lastKey = Object.keys(this.subscribers).at(-1)
+    const newKey = (lastKey as unknown as number || 0) + 1
+    this.subscribers[newKey] = fn
+    return newKey
+  }
+
+  unsub (key: number) {
+    delete this.subscribers[key]
+  }
+
+  setValue (fn: SetAtomState<T>) {
+    this.value = fn(this.value)
+    Object.keys(this.subscribers).map((x) => {
+      this.subscribers[x as unknown as number](this.value)
+    })
+  }
+}
+
+export const useAtomValue = <T>(atom: Atom<T>) => {
+  const [state, setState] = useState(atom.value)
+  const subRef = useRef<number>()
+  useEffect(() => {
+    subRef.current = atom.subscribeAnyFunction((x) => {
+      setState(x)
+    })
+    return () => {
+      subRef.current && atom.unsub(subRef.current)
+    }
+  }, [atom])
+
+  return state
+}
+export const useAtomSetState = <T>(atom: Atom<T>) => {
+  const update = useCallback(
+    (x) => {
+      atom.setValue(x)
+    },
+    [atom],
+  )
+  return update
+}
+export const useAtomState = <T>(atom: Atom<T>) => {
+  const [state, setState] = useState(atom.value)
+  const subRef = useRef<number>()
+  useEffect(() => {
+    subRef.current = atom.subscribeAnyFunction((x) => {
+      setState(x)
+    })
+    return () => {
+      subRef.current && atom.unsub(subRef.current)
+    }
+  }, [atom])
+  const update = useCallback(
+    (x) => {
+      atom.setValue(x)
+    },
+    [atom],
+  )
+  return [state, update]
+}
